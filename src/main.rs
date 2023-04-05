5fdef0e22462f268aa9d14b65e9502d7735b1007
Fitti <dev@fitti.io>
2023-04-05 12:18:36 +0200

    First commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use clap::{arg, Command};
+use std::env;
+use std::path::{Path, PathBuf};
+use std::process;
+
+fn main() {
+    let cmd = Command::new("hack").args(&[arg!(<name> "The name of the hack"), arg!(-t - -temp)]);
+    let matches = cmd.get_matches();
+    let name = matches.get_one::<String>("name").unwrap();
+    let temp = *matches.get_one::<bool>("temp").unwrap();
+    let mut path = Path::new("/").join(if temp {
+        "tmp".into()
+    } else {
+        dirs::home_dir().unwrap().join("projects").join("hacks")
+    });
+    change_dir(&path);
+    path = path.join(name);
+    make_hack(&path);
+    println!("{}", path.to_str().unwrap());
+}
+
+fn make_hack(path: &PathBuf) {
+    if !path.try_exists().unwrap() {
+        process::Command::new("cargo")
+            .arg("new")
+            .arg(path.file_name().unwrap())
+            .output()
+            .unwrap();
+    } else {
+        panic!("directory already exists");
+    };
+}
+
+fn change_dir<T: AsRef<Path>>(path: T) {
+    env::set_current_dir(path).unwrap();
+}
