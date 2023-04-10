c40eb9210d18136e3d42093d4caa1a4b9705a446
Fitti <dev@fitti.io>
2023-04-10 13:11:17 +0200

    Make hack optional

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,22 @@ use std::path::{Path, PathBuf};
 use std::process;
 
 fn main() {
-    let cmd = Command::new("hack").args(&[arg!(<name> "The name of the hack"), arg!(-t - -temp)]);
+    let cmd = Command::new("hack").args(&[
+        arg!(<name> "The name of the hack"),
+        arg!(-t - -temp),
+        arg!(-h - -hack),
+    ]);
     let matches = cmd.get_matches();
     let name = matches.get_one::<String>("name").unwrap();
     let temp = *matches.get_one::<bool>("temp").unwrap();
+    let hack = *matches.get_one::<bool>("hack").unwrap();
     let mut path = Path::new("/").join(if temp {
         "tmp".into()
     } else {
-        dirs::home_dir().unwrap().join("projects").join("hacks")
+        dirs::home_dir()
+            .unwrap()
+            .join("projects")
+            .join(if hack { "hacks" } else { "" })
     });
     change_dir(&path);
     path = path.join(name);
